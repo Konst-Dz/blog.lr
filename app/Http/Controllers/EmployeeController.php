e6aadad0d661dca3da89bcde394fc6d1a823a3e8
Konst-Dz <kanstantsinas@gmail.com>
2020-06-08 16:34:38 +0300

    practice of Blade

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -50,10 +50,56 @@ class EmployeeController extends Controller
         return $this->employees[$id][$param];
     }
 
+
     public function showTwo()
     {
-        return view('employee.new');
+        return view('employee.new',['title'=>'new',
+            'aside'=>'add entry',
+            'links'=>[
+                        ['text'=>'text1',
+                          'href'=>'href1'
+                        ],
+                        ['text'=>'text2',
+                            'href'=>'href2'
+                        ],
+                        ['text'=>'text3',
+                            'href'=>'href3'
+                        ],
+
+                    ],
+            'employees' => [
+                [
+                    'name' => 'user1',
+                    'surname' => 'surname1',
+                    'salary' => 1000,
+                ],
+                [
+                    'name' => 'user2',
+                    'surname' => 'surname2',
+                    'salary' => 2000,
+                ],
+                [
+                    'name' => 'user3',
+                    'surname' => 'surname3',
+                    'salary' => 3000,
+                ],
+                [
+                    'name' => 'user4',
+                    'surname' => 'surname4',
+                    'salary' => 4000,
+                ],
+                [
+                    'name' => 'user5',
+                    'surname' => 'surname5',
+                    'salary' => 5000,
+                ],
+            ],
+
+
+            ]);
     }
+
+
     public function show($city = 'minsk',$year = '1123')
     {
         $tbl = [];
--- resources/views/employee/new.blade.php
+++ resources/views/employee/new.blade.php
@@ -1,5 +1,38 @@
-@extends('layouts.layout')
+@extends('layouts.app')
+
+@section('title', $title )
 
 @section('content')
     <p>Это содержимое тела страницы.</p>
+@endsection
+
+
+@section('sidebar')
+    @parent
+    <p>{{$aside}}.</p>
+    @foreach($links as $item)
+        <a href="{{$item['href']}}">{{$item['text']}}</a><br>
+    @endforeach
+    <ul>
+        @foreach($links as $item)
+            <li><a href="{{$item['href']}}">{{$item['text']}}</a></li>
+        @endforeach
+    </ul>
+    <table>
+        <tr>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Зарплата</th>
+        </tr>
+        @foreach ($employees as $value)
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                @foreach ($value as $elem)
+                    @if()
+                    <td>{{ $elem }}</td>
+                    @endif
+                @endforeach
+            </tr>
+        @endforeach
+    </table>
 @endsection
